79bd3bc364b70b3de20d916d0d00f9140c34d09c
asp <asp@aptechnn.edu>
2016-06-20 16:09:39 +0500

    aaasa

---
+++ toyota cars/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace toyota_cars
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        int change = 1;
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainPage()
+        {
+           
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Start();
+            timer.Tick += timer_tick;
+            this.InitializeComponent();
+            this.DataContext = itemsss.GetTours();
+        }
+
+        private void timer_tick(object sender, object e)
+        {
+            int newindex = flip.SelectedIndex + change;
+            if (newindex >= flip.Items.Count || newindex < 0) { change *= -1; }
+            flip.SelectedIndex += change;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(intro));
+        }
+
+        
+
+
+      
+    }
+}
---
+++ toyota cars/itemsss.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toyota_cars
+{
+partial class itemsss
+    {
+       public static ObservableCollection<tour_data> GetTours(){
+             
+            var temp = new ObservableCollection<tour_data>();
+            tour_data tour;
+            tour = new tour_data
+             //{    tour_image = "ms-appx:///Assets/1.jpg",
+             //    tour_text = "Samsung",
+             //    tour_description = "is the largest mobile phone maker in its home market of South Korea, and the third largest in the world. In addition to mobile phones and related devices, the company also manufacturers things such as televisions, cameras, and electronic components"
+             //};
+            //temp.Add(tour);
+            //tour = new tour_data
+            //{
+
+            //    tour_text = "Samsung",
+            //    tour_image = "ms-appx:///Assets/2.jpg",
+            //    tour_description = "is the largest mobile phone maker in its home market of South Korea, and the third largest in the world. In addition to mobile phones and related devices, the company also manufacturers things such as televisions, cameras, and electronic components"
+            //};
+            // temp.Add(tour);
+            //tour = new tour_data
+              {
+                tour_text = "LG",
+                tour_image = "ms-appx:///Assets/3.jpg",
+                tour_description = "is a South Korean company that manufactures mobile phones, as well as other devices such as televisions and home appliances. LG originally stood for Lucky Goldstar, but is now promoted as meaning Life's Good in the company's marketing campaigns."
+            };
+            temp.Add(tour);
+            tour = new tour_data
+            
+              {
+                tour_text = "LG",
+                tour_image = "ms-appx:///Assets/4.jpg",
+                tour_description = "is a South Korean company that manufactures mobile phones, as well as other devices such as televisions and home appliances. LG originally stood for Lucky Goldstar, but is now promoted as meaning Life's Good in the company's marketing campaigns."
+            };
+               temp.Add(tour);
+            tour = new tour_data
+              {
+                tour_text = "LG",
+                tour_image = "ms-appx:///Assets/5.jpg",
+                tour_description = "is a South Korean company that manufactures mobile phones, as well as other devices such as televisions and home appliances. LG originally stood for Lucky Goldstar, but is now promoted as meaning Life's Good in the company's marketing campaigns."
+            };
+            temp.Add(tour);
+
+            return temp;
+        
+        }
+    }
+    class tour_data
+    {
+
+        public string tour_image { get; set; }
+
+        public string tour_text { get; set; }
+
+        public string tour_description { get; set; }
+    }
+}
